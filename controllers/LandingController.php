0776a30899126fc5ef8c963ddce5022ad41ce95e
Santiago Carrillo <juniorcarrilo14@gmail.com>
2025-03-24 11:09:06 +0100

    adjust responsive landing page

--- controllers/LandingController.php
+++ controllers/LandingController.php
@@ -9,6 +9,8 @@ use MVC\Router;
 class LandingController {
     public static function index(Router $router)
     {
-        $router->render('landing/index', []);
+        $router->render('landing/index', [
+            'titulo' => 'Landing Page'
+        ]);
     }
 }
